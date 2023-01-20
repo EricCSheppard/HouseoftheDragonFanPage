6f606e8fd0809911da6b28e0e114c54d57aa3bec
EricCSheppard <EricCSheppard@gmail.com>
2023-01-20 09:02:05 -0500

    Adds function to bio button

--- js/app.js
+++ js/app.js
@@ -1,2 +1,51 @@
 console.log('hiya world')
 
+RhaenyraBio = document.getElementById('Rhaenyrabio')
+RhaenyraBio.addEventListener("click", printRhaenyraBio)
+
+function printRhaenyraBio() {
+    document.getElementById("info").innerHTML = "Rhaenyra Bio goes here"
+}
+
+RhaenyraBio = document.getElementById('Rhaenyrastatus')
+RhaenyraBio.addEventListener("click", printRhaenyraBio)
+
+function printRhaenyraBio() {
+    document.getElementById("info").innerHTML = "Rhaenyra Bio goes here"
+}
+
+AlicentBio = document.getElementById('Alicentbio')
+AlicentBio.addEventListener("click", printAlicentBio)
+
+function printAlicentBio() {
+    document.getElementById("info").innerHTML = "Alicent Bio goes here"
+}
+
+ViserysBio = document.getElementById('Viserysbio')
+ViserysBio.addEventListener("click", printViserysBio)
+
+function printViserysBio() {
+    document.getElementById("info").innerHTML = "Viserys Bio goes here"
+}
+
+DaemonBio = document.getElementById('Daemonbio')
+DaemonBio.addEventListener("click", printDaemonBio)
+
+function printDaemonBio() {
+    document.getElementById("info").innerHTML = "Daemon Bio goes here"
+}
+
+LaenorBio = document.getElementById('Laenorbio')
+LaenorBio.addEventListener("click", printLaenorBio)
+
+function printLaenorBio() {
+    document.getElementById("info").innerHTML = "Laenor Bio goes here"
+}
+LaenaBio = document.getElementById('Laenabio')
+LaenaBio.addEventListener("click", printLaenaBio)
+
+function printLaenaBio() {
+    document.getElementById("info").innerHTML = "Laena Bio goes here"
+}
+
+
