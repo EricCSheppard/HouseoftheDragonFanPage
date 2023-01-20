6f9b47d3591fa11d48d7424dc9e21762f22b8c29
EricCSheppard <EricCSheppard@gmail.com>
2023-01-20 09:17:31 -0500

    all buttons working

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ function printRhaenyraBio() {
     document.getElementById("info").innerHTML = "Rhaenyra Bio goes here"
 }
 
-RhaenyraBio = document.getElementById('Rhaenyrastatus')
-RhaenyraBio.addEventListener("click", printRhaenyraBio)
+RhaenyraStatus = document.getElementById('Rhaenyrastatus')
+RhaenyraStatus.addEventListener("click", printRhaenyraStatus)
 
-function printRhaenyraBio() {
-    document.getElementById("info").innerHTML = "Rhaenyra Bio goes here"
+function printRhaenyraStatus() {
+    document.getElementById("RTstatus").innerHTML = "Alive"
 }
 
 AlicentBio = document.getElementById('Alicentbio')
@@ -21,11 +21,25 @@ function printAlicentBio() {
     document.getElementById("info").innerHTML = "Alicent Bio goes here"
 }
 
+AlicentStatus = document.getElementById('Alicentstatus')
+AlicentStatus.addEventListener("click", printAlicentStatus)
+
+function printAlicentStatus() {
+    document.getElementById("AHstatus").innerHTML = "Alive"
+}
+
 ViserysBio = document.getElementById('Viserysbio')
 ViserysBio.addEventListener("click", printViserysBio)
 
 function printViserysBio() {
     document.getElementById("info").innerHTML = "Viserys Bio goes here"
+}
+
+ViserysStatus = document.getElementById('Viserysstatus')
+ViserysStatus.addEventListener("click", printViserysStatus)
+
+function printViserysStatus() {
+    document.getElementById("VTstatus").innerHTML = "Dead"
 }
 
 DaemonBio = document.getElementById('Daemonbio')
@@ -35,17 +49,39 @@ function printDaemonBio() {
     document.getElementById("info").innerHTML = "Daemon Bio goes here"
 }
 
+DaemonStatus = document.getElementById('Daemonstatus')
+DaemonStatus.addEventListener("click", printDaemonStatus)
+
+function printDaemonStatus() {
+    document.getElementById("DTstatus").innerHTML = "Alive"
+}
+
 LaenorBio = document.getElementById('Laenorbio')
 LaenorBio.addEventListener("click", printLaenorBio)
 
 function printLaenorBio() {
     document.getElementById("info").innerHTML = "Laenor Bio goes here"
 }
+
+LaenorStatus = document.getElementById('Laenorstatus')
+LaenorStatus.addEventListener("click", printLaenorStatus)
+
+function printLaenorStatus() {
+    document.getElementById("LV1status").innerHTML = "Alive"
+}
+
 LaenaBio = document.getElementById('Laenabio')
 LaenaBio.addEventListener("click", printLaenaBio)
 
 function printLaenaBio() {
     document.getElementById("info").innerHTML = "Laena Bio goes here"
+}
+
+LaenaStatus = document.getElementById('Laenastatus')
+LaenaStatus.addEventListener("click", printLaenaStatus)
+
+function printLaenaStatus() {
+    document.getElementById("LV2status").innerHTML = "Dead"
 }
 
 
