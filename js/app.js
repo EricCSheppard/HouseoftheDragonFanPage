5d89b7d9c977aec1564d1937ff2dc3aabf20b2a0
EricCSheppard <EricCSheppard@gmail.com>
2023-01-20 09:45:50 -0500

    changes css style, search bar non-functional

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ RhaenyraBio = document.getElementById('Rhaenyrabio')
 RhaenyraBio.addEventListener("click", printRhaenyraBio)
 
 function printRhaenyraBio() {
-    document.getElementById("info").innerHTML = "Rhaenyra Bio goes here"
+    document.getElementById("info").innerHTML = "Rhaenyra Targaryen, cheered as the Realm's Delight when she was young, and later called the Half-Year Queen,[7] was the first-born child of King Viserys I Targaryen. Rhaenyra was King Viserys I's only living child by his first wife, Queen Aemma of House Arryn. At the age of eight, she was made her father's heir, and grew up expecting to become the first ruling Queen of Westeros. Rhaenyra was a dragonrider whose mount was Syrax. <br><br> Her claim to the Iron Throne was challenged by her younger half-brother, Aegon II Targaryen, sparking the civil war known as the Dance of the Dragons. She was the mother of two kings,[11] Aegon III and Viserys II.<br><br> Rhaenyra's personal sigil was the standard Targaryen heraldry (a three-headed red dragon on a black field), quartered with the moon-and-falcon sigil of House Arryn (for her mother Aemma Arryn), and the silver seahorse on sea green of House Velaryon (for her first husband Laenor Velaryon, as well as for the Velaryons in general, who were among her major supporters). During the Dance, her faction used her personal heraldry as their battle-flag"
 }
 
 RhaenyraStatus = document.getElementById('Rhaenyrastatus')
